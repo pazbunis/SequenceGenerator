47552a948b9552174adcb138036f1bd53750c760
Paz Bunis <pazbunis@gmail.com>
2016-08-07 17:05:28 +0300

    before serialization

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ replaced_dna = gen.random_expression_replacement(dna, 'AAAAAA')
 
 print(dna)
 print(replaced_dna)
-
 print(gen.dna_to_one_hot(dna))
+
+
+def generate_sample(m, x_path, y_path):
+    """generates m sample points and stores them at the given paths, line delimited"""
+
 
