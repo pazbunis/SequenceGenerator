2b60b18e0b68dc9c105d41fbcbfd14d25b137054
Paz Bunis <pazbunis@gmail.com>
2016-08-07 15:59:41 +0300

    small changes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import generator as gen
-dna = gen.generate_random_dna([0.25, 0.25, 0.25, 0.25], 30)
+dna = gen.generate_random_dna([0.25, 0.25, 0.25, 0.25], 15)
+replaced_dna = gen.random_expression_replacement(dna, 'AAAAAA')
+
 print(dna)
-#print(gen.dna_to_one_hot(dna))
-rep_dna = gen.random_expression_replacement(dna, 'AAAAAA')
-print(rep_dna)
+print(replaced_dna)
+
+print(gen.dna_to_one_hot(dna))
