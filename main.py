ec83a7d93b2042fba5d955e7d34f6ea1a02d4218
Paz Bunis <pazbunis@gmail.com>
2016-08-09 13:31:32 +0300

    generating more sequences to improve accuracy

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ def generate_sample(half_size, x_path, y_path):
     with open(x_path, 'w') as x_file:
         with open(y_path, 'w') as y_file:
             for i in range(0, 2 * half_size):
-                dna = gen.generate_random_dna([0.25, 0.25, 0.25, 0.25], 1000)
+                dna = gen.generate_random_dna([0.25, 0.25, 0.25, 0.25], 100)
                 if labels[i] == 1:
                     #dna = gen.place_expression(dna, 'AAAAAAAA', 0)
                     dna = gen.random_expression_replacement(dna, 'AAAAAAAA')
                 x_file.write(dna + '\n')
                 y_file.write(str(labels[i]) + '\n')
 
-train_x_path = '/cs/grad/pazbu/paz/dev/projects/SequenceGenerator/train_x_1000.txt'
-train_y_path = '/cs/grad/pazbu/paz/dev/projects/SequenceGenerator/train_y_1000.txt'
+train_x_path = '/cs/grad/pazbu/paz/dev/projects/SequenceGenerator/train_x_100.txt'
+train_y_path = '/cs/grad/pazbu/paz/dev/projects/SequenceGenerator/train_y_100.txt'
 
-test_x_path = '/cs/grad/pazbu/paz/dev/projects/SequenceGenerator/test_x_1000.txt'
-test_y_path = '/cs/grad/pazbu/paz/dev/projects/SequenceGenerator/test_y_1000.txt'
+test_x_path = '/cs/grad/pazbu/paz/dev/projects/SequenceGenerator/test_x_100.txt'
+test_y_path = '/cs/grad/pazbu/paz/dev/projects/SequenceGenerator/test_y_100.txt'
 
 
-generate_sample(1000, train_x_path, train_y_path)
+generate_sample(10000, train_x_path, train_y_path)
 generate_sample(100, test_x_path, test_y_path)
